a7dd2a15052a158688d403a22aadb2b7a5c23a5c
honggangli <lihonggang24@gmail.com>
2012-07-18 10:46:21 +0800

    定义adapter对象(当前为匿名函数，没有自定义任何属性、方法)

---
+++ adapter.js
@@ -0,0 +1,25 @@
+﻿(function(self){
+   var _adapter = self.adapter;
+   var adapter = function(){};
+   if(isNodejs()){
+     log("server");
+     var cmdArray = process.argv.slice(2,process.argv.length);
+     log(cmdArray);
+   }else if(isBrowser()){
+     log("client");
+     var url = window.loaction.href;
+     log(url);
+   }
+   self.adapter = adapter;
+
+   function isNodejs() {
+        return typeof exports !== 'undefined' && typeof module !== 'undefined' && typeof module.exports !== 'undefined';
+    }
+    function isBrowser() {
+        return typeof window !== 'undefined' && typeof document !== 'undefined';
+    }
+    function log(){
+      if(console&&console.log)
+        console.log.apply(console,arguments);
+    }
+})(this)
